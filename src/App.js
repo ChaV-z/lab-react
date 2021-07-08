bef5d85669e5f246000adc8b9de50f4980bc1e93
Christmas <yong.christmas@gmail.com>
2021-07-08 10:15:48 +0700

    15 min

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import CharacterCard from './CharacterCard';
+const word = "Hello";
 function App() {
   return (
-    <div className='App'>
-      Hello {"World"}
+    <div>
+      { Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
     </div>
   );
 }
+
 export default App;
 
