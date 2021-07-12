712b4012e9f7e3f76974c9c2089307ea9f8ba57f
Christmas <yong.christmas@gmail.com>
2021-07-12 23:37:28 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
+import Header from './Header';
 
 const word = "Hello";
 function App() {
   return (
-    <div>
-      <WordCard value={word}/>
-    </div>
-  );
+		<div>
+			<Header />
+			<WordCard value={word}/>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <h1>What's the word?</h1>
+            <p>Your mission : Sort the words correctly!</p>
+        </>
+    )
+}
+
+export default Header
