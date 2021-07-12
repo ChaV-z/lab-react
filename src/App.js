4a5f313fc9c32c049c766053206740b24b5219c8
Christmas <yong.christmas@gmail.com>
2021-07-12 23:39:03 +0700

    add word and show word that random

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import WordCard from './WordCard';
 import Header from './Header';
 
-const word = "Hello";
+const words = ["Hello", "React", "JavaScript", "html", "json"];
+let selectedWord = words[Math.floor(Math.random()*words.length)];
 function App() {
   return (
 		<div>
 			<Header />
-			<WordCard value={word}/>
+			<WordCard value={selectedWord}/>
 		</div>
 	);
 }
